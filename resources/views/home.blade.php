604077c69c732a53577156f0bc83a3fe2adac673
Alexlytle <44213950+Alexlytle@users.noreply.github.com>
2022-01-25 18:13:53 -0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,13 +20,7 @@
                     @endif
 
                     <table>
-                        @foreach ($invoices as $invoice)
-                            <tr>
-                                <td>{{ $invoice->date()->toFormattedDateString() }}</td>
-                                <td>{{ $invoice->total() }}</td>
-                                <td><a href="/user/invoice/{{ $invoice->id }}">Download</a></td>
-                            </tr>
-                        @endforeach
+              
                     </table>
 
                     Welcome to the Dashboard
